a51e1e61fd6c1141e358c016166838b11e59c748
Gvelecico <101369072+Gvelecico@users.noreply.github.com>
2022-10-12 19:41:51 -0300

    Add files via upload

---
+++ Process.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Process {
+   Integer pid ;
+   Integer duration ;
+   Integer arrival ;
+   Integer io ;
+}
+
+
+
+
